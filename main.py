c9287e79274db5d7077925a293e048d1665a63cd
Elyse Kriegel <esamkriegel@gmail.com>
2021-05-21 17:57:15 -0500

    added a bash script to run the bot

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
-import re
 from termcolor import colored
 from gpu_bot.spiders.stock_check import stockCheckSpider 
+import time
+
 
 def main():
-    process = CrawlerProcess()
+    process  = CrawlerProcess()
     process.crawl(stockCheckSpider)
-    
-    data = process.start()
+    process.start()
 
     with open('gamestop_stock.json', 'r+') as stock:
         print(colored(stock.readlines()[1], 'blue'))
---
+++ run_bot.sh
@@ -0,0 +1,4 @@
+while true; do
+	pipenv run python main.py 
+	sleep 10
+done 
